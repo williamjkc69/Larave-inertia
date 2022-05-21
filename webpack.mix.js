310daedacd79550c86c047b5d22a4a00dba27874
Williamjkc69 <williamjkc69@gmail.com>
2022-05-21 09:29:07 -0600

    first commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,21 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.browserSync({
+    proxy: "http://127.0.0.1:8000",
+});
+
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .postCss("resources/css/app.css", "public/css", [
+        require("postcss-import"),
+        require("tailwindcss"),
+        require("autoprefixer"),
+    ])
+    .alias({
+        "@": "resources/js",
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
